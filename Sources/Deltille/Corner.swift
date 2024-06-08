3020f5594cdf5de8b57e39ef46defff6ea075736
zilmarinen <zack@zackbrown.co.uk>
2024-06-08 13:16:02 +0100

    Add hexagon corner and edge enums

--- Sources/Deltille/Corner.swift
+++ Sources/Deltille/Corner.swift
@@ -46,3 +46,42 @@ extension Grid.Triangle {
         }
     }
 }
+
+extension Grid.Hexagon {
+    
+    ///
+    /// A corner defines one of six vertices of a hexagon.
+    ///
+    
+    public enum Corner: Int,
+                        CaseIterable {
+        
+        case c0, c1, c2, c3, c4, c5
+        
+        public var connected: [Corner] {
+            
+            switch self {
+                
+            case .c0: return [.c1, .c5]
+            case .c1: return [.c2, .c0]
+            case .c2: return [.c3, .c1]
+            case .c3: return [.c4, .c2]
+            case .c4: return [.c5, .c3]
+            case .c5: return [.c0, .c4]
+            }
+        }
+        
+        public var edges: [Edge] {
+            
+            switch self {
+                
+            case .c0: return [.e0, .e5]
+            case .c1: return [.e1, .e0]
+            case .c2: return [.e2, .e1]
+            case .c3: return [.e3, .e2]
+            case .c4: return [.e4, .e3]
+            case .c5: return [.e5, .e4]
+            }
+        }
+    }
+}
--- Sources/Deltille/Edge.swift
+++ Sources/Deltille/Edge.swift
@@ -36,3 +36,42 @@ extension Grid.Triangle {
         }
     }
 }
+
+extension Grid.Hexagon {
+    
+    ///
+    /// An edge defines one of six sides of a hexagon.
+    ///
+    
+    public enum Edge: Int,
+                      CaseIterable {
+        
+        case e0, e1, e2, e3, e4, e5
+        
+        public var connected: [Edge] {
+            
+            switch self {
+                
+            case .e0: return [.e1, .e5]
+            case .e1: return [.e2, .e0]
+            case .e2: return [.e3, .e1]
+            case .e3: return [.e4, .e2]
+            case .e4: return [.e5, .e3]
+            case .e5: return [.e0, .e4]
+            }
+        }
+        
+        public var corners: [Corner] {
+            
+            switch self {
+                
+            case .e0: return [.c1, .c0]
+            case .e1: return [.c2, .c1]
+            case .e2: return [.c3, .c2]
+            case .e3: return [.c4, .c3]
+            case .e4: return [.c5, .c4]
+            case .e5: return [.c0, .c5]
+            }
+        }
+    }
+}
