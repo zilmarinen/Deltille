78714aee1d16432b124305e087ea9ae4666d6a82
Zack Brown <zack@zackbrown.co.uk>
2024-12-15 11:38:47 +0000

    Refactor corner and edge protocols

--- Sources/Deltille/Corner.swift
+++ Sources/Deltille/Corner.swift
@@ -4,3 +4,86 @@
 //  Created by Zack Brown on 23/05/2024.
 //
 
+// MARK: Corner
+
+public protocol Corner: CaseIterable,
+                        Codable,
+                        Equatable,
+                        Hashable,
+                        RawRepresentable,
+                        Sendable {
+    
+    associatedtype E = Edge
+    
+    var corners: [Self] { get }
+    
+    var edges: [E] { get }
+}
+
+// MARK: Triangle
+
+extension Grid.Triangle {
+    
+    public enum Corner: Int,
+                        Deltille.Corner {
+        
+        case c0, c1, c2
+        
+        public var corners: [Corner] {
+                    
+            switch self {
+                
+            case .c0: return [.c1, .c2]
+            case .c1: return [.c2, .c0]
+            case .c2: return [.c0, .c1]
+            }
+        }
+        
+        public var edges: [Edge] {
+            
+            switch self {
+                
+            case .c0: return [.e0, .e2]
+            case .c1: return [.e1, .e0]
+            case .c2: return [.e2, .e1]
+            }
+        }
+    }
+}
+
+// MARK: Hexagon
+
+extension Grid.Hexagon {
+    
+    public enum Corner: Int,
+                        Deltille.Corner {
+        
+        case c0, c1, c2, c3, c4, c5
+        
+        public var corners: [Corner] {
+            
+            switch self {
+                
+            case .c0: return [.c1, .c5]
+            case .c1: return [.c2, .c0]
+            case .c2: return [.c3, .c1]
+            case .c3: return [.c4, .c2]
+            case .c4: return [.c5, .c3]
+            case .c5: return [.c0, .c4]
+            }
+        }
+        
+        public var edges: [Edge] {
+            
+            switch self {
+                
+            case .c0: return [.e0, .e5]
+            case .c1: return [.e1, .e0]
+            case .c2: return [.e2, .e1]
+            case .c3: return [.e3, .e2]
+            case .c4: return [.e4, .e3]
+            case .c5: return [.e5, .e4]
+            }
+        }
+    }
+}
--- Sources/Deltille/Edge.swift
+++ Sources/Deltille/Edge.swift
@@ -4,3 +4,86 @@
 //  Created by Zack Brown on 24/05/2024.
 //
 
+// MARK: Edge
+
+public protocol Edge: CaseIterable,
+                      Codable,
+                      Equatable,
+                      Hashable,
+                      RawRepresentable,
+                      Sendable {
+    
+    associatedtype C = Corner
+    
+    var corners: [C] { get }
+    
+    var edges: [Self] { get }
+}
+
+// MARK: Triangle
+
+extension Grid.Triangle {
+    
+    public enum Edge: Int,
+                      Deltille.Edge {
+        
+        case e0, e1, e2
+        
+        public var corners: [Corner] {
+            
+            switch self {
+                
+            case .e0: return [.c1, .c0]
+            case .e1: return [.c2, .c1]
+            case .e2: return [.c0, .c2]
+            }
+        }
+        
+        public var edges: [Edge] {
+           
+            switch self {
+               
+            case .e0: return [.e1, .e2]
+            case .e1: return [.e2, .e0]
+            case .e2: return [.e0, .e1]
+            }
+        }
+    }
+}
+
+// MARK: Hexagon
+
+extension Grid.Hexagon {
+    
+    public enum Edge: Int,
+                      Deltille.Edge {
+        
+        case e0, e1, e2, e3, e4, e5
+        
+        public var corners: [Corner] {
+            
+            switch self {
+                
+            case .e0: return [.c1, .c0]
+            case .e1: return [.c2, .c1]
+            case .e2: return [.c3, .c2]
+            case .e3: return [.c4, .c3]
+            case .e4: return [.c5, .c4]
+            case .e5: return [.c0, .c5]
+            }
+        }
+        
+        public var edges: [Edge] {
+            
+            switch self {
+                
+            case .e0: return [.e1, .e5]
+            case .e1: return [.e2, .e0]
+            case .e2: return [.e3, .e1]
+            case .e3: return [.e4, .e2]
+            case .e4: return [.e5, .e3]
+            case .e5: return [.e0, .e4]
+            }
+        }
+    }
+}
