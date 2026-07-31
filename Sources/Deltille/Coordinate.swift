71c958f103e65bb2e70d6b1ec34670c24152a268
Zack Brown <zack@zackbrown.co.uk>
2026-07-31 11:15:09 +0100

    Promote Coordinate to protocol

--- Sources/Deltille/Coordinate.swift
+++ Sources/Deltille/Coordinate.swift
@@ -1,54 +1,79 @@
 //
 //  Coordinate.swift
+//  Deltille
 //
 //  Created by Zack Brown on 23/05/2024.
 //
 
+import Euclid
+import Foundation
+
 // MARK: Coordinate
 
-public struct Coordinate: Codable,
-                          Equatable,
-                          Hashable,
-                          Identifiable,
-                          Sendable {
+public protocol Coordinate: Codable,
+                            Equatable,
+                            Hashable,
+                            Identifiable,
+                            Sendable {
     
-    public static let zero = Self(0, 0, 0)
-    public static let one = Self(1, 1, 1)
-    public static let unitX = Self(1, 0, 0)
-    public static let unitY = Self(0, 1, 0)
-    public static let unitZ = Self(0, 0, 1)
+    var x: Int { get }
+    var y: Int { get }
+    var z: Int { get }
     
-    public let x: Int
-    public let y: Int
-    public let z: Int
+    init(_ x: Int,
+         _ y: Int,
+         _ z: Int)
     
-    public var id: String { "[\(x), \(y), \(z)]" }
+    init(_ vector: Vector,
+         _ scale: Double)
     
-    public var sum: Int { x + y + z }
-            
-    public var equalToZero: Bool { sum == 0 }
-    public var equalToOne: Bool { sum == 1 }
-    public var equalToNegativeOne: Bool { sum == -1 }
+    var id: String { get }
     
-    public init(_ x: Int,
-                _ y: Int,
-                _ z: Int) {
+    var sum: Int { get }
+    var equalToZero: Bool { get }
+    var equalToOne: Bool { get }
+    var equalToNegativeOne: Bool { get }
+    
+    var adjacent: [Self] { get }
+    
+    var vector: Vector { get }
+    
+    var xyz: (x: Int,
+              y: Int,
+              z: Int) { get }
+    
+    func distance(_ other: Self) -> Int
+}
+
+public extension Coordinate {
+ 
+    static var zero: Self {
         
-        self.x = x
-        self.y = y
-        self.z = z
+        .init(0, 0, 0)
+    }
+    
+    static var one: Self {
+        
+        .init(1, 1, 1)
+    }
+    
+    static var unitX: Self {
+        
+        .init(1, 0, 0)
+    }
+    
+    static var unitY: Self {
+        
+        .init(0, 1, 0)
+    }
+    
+    static var unitZ: Self {
+        
+        .init(0, 0, 1)
     }
 }
 
 public extension Coordinate {
-    
-    static func -(lhs: Self,
-                  rhs: Self) -> Self {
-        
-        .init(lhs.x - rhs.x,
-              lhs.y - rhs.y,
-              lhs.z - rhs.z)
-    }
     
     static func +(lhs: Self,
                   rhs: Self) -> Self {
@@ -56,6 +81,14 @@ public extension Coordinate {
         .init(lhs.x + rhs.x,
               lhs.y + rhs.y,
               lhs.z + rhs.z)
+    }
+    
+    static func -(lhs: Self,
+                  rhs: Self) -> Self {
+        
+        .init(lhs.x - rhs.x,
+              lhs.y - rhs.y,
+              lhs.z - rhs.z)
     }
     
     static prefix func -(rhs: Self) -> Self {
@@ -73,12 +106,74 @@ public extension Coordinate {
               lhs.z * rhs) }
 }
 
+
 public extension Coordinate {
+    
+    var id: String {
+        
+        "[\(x), \(y), \(z)]"
+    }
+    
+    var sum: Int {
+        
+        x + y + z
+    }
+    
+    var equalToZero: Bool {
+        
+        sum == 0
+    }
+    
+    var equalToOne: Bool {
+        
+        sum == 1
+    }
+    
+    
+    var equalToNegativeOne: Bool {
+        
+        sum == -1
+    }
+    
+    var adjacent: [Self] {
+        
+        [self + .unitX,
+         self - .unitZ,
+         self + .unitY,
+         self - .unitX,
+         self + .unitZ,
+         self - .unitY]
+    }
+    
+    var vector: Vector {
+        
+        .init(self)
+    }
     
     var xyz: (x: Int,
               y: Int,
               z: Int) {
         
         (x, y, z)
+    }
+}
+
+public extension Coordinate {
+    
+    init(_ vector: Vector,
+         _ scale: Double = 1.0) {
+        
+        let quantised = vector.quantised(scale)
+        
+        self.init(quantised.x,
+                  quantised.y,
+                  quantised.z)
+    }
+    
+    func distance(_ other: Self) -> Int {
+        
+        (abs(x - other.x) +
+         abs(y - other.y) +
+         abs(z - other.z))
     }
 }
